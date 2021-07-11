ca2138c1593e4c839b18f57de7cc39da15c79dac
Riccardo Perra <riccardo.perra@icloud.com>
2021-07-11 12:09:06 +0200

    feat: add some loading observable creator, add type-safe guards

--- projects/ngx-reactive-loading/src/lib/model/loading.ts
+++ projects/ngx-reactive-loading/src/lib/model/loading.ts
@@ -1,4 +1,5 @@
-import { MonoTypeOperatorFunction, Observable } from 'rxjs';
+import { isObservable, MonoTypeOperatorFunction, Observable } from 'rxjs';
+import { isFunction } from 'rxjs/internal-compatibility';
 
 export type LoadingStoreState = {
   readonly loading$: Observable<boolean>;
@@ -7,4 +8,26 @@ export type LoadingStoreState = {
 
 export type LoadingStore<K extends readonly [...PropertyKey[]]> = {
   [Key in K[number]]: Readonly<LoadingStoreState>;
+};
+
+export const isLoadingStoreState = (
+  value: unknown
+): value is LoadingStoreState => {
+  return (
+    typeof value === 'object' &&
+    !!value &&
+    'loading$' in value &&
+    isObservable((value as Partial<LoadingStoreState>).loading$) &&
+    isFunction((value as Partial<LoadingStoreState>).track)
+  );
+};
+
+export const isLoadingStore = (
+  value: unknown
+): value is LoadingStore<PropertyKey[]> => {
+  return (
+    typeof value === 'object' &&
+    !!value &&
+    Object.values(value).every(value => isLoadingStoreState(value))
+  );
 };
---
+++ projects/ngx-reactive-loading/src/lib/operators/some-loading.ts
@@ -0,0 +1,42 @@
+import {
+  isLoadingStore,
+  isLoadingStoreState,
+  LoadingStore,
+  LoadingStoreState,
+} from '../model/loading';
+import { combineLatest, isObservable, Observable } from 'rxjs';
+import { distinctUntilChanged, map, shareReplay } from 'rxjs/operators';
+
+export const someLoading = <
+  Loaders extends readonly [
+    ...(readonly (
+      | LoadingStore<readonly PropertyKey[]>
+      | LoadingStoreState
+      | Observable<boolean>
+    )[])
+  ]
+>(
+  loaders: Loaders
+) => {
+  const loaders$: Observable<boolean>[] = loaders.reduce<Observable<boolean>[]>(
+    (acc, loader) => {
+      if (isObservable(loader)) {
+        return acc.concat(loader);
+      }
+      if (isLoadingStoreState(loader)) {
+        return acc.concat(loader.loading$);
+      }
+      if (isLoadingStore(loader)) {
+        return acc.concat(Object.values(loader).map(state => state.loading$));
+      }
+      return acc;
+    },
+    []
+  );
+
+  return combineLatest(loaders$).pipe(
+    map(loaders => loaders.some(loader => loader)),
+    distinctUntilChanged(),
+    shareReplay({ refCount: true, bufferSize: 1 })
+  );
+};

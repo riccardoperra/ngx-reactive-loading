85d75cd12adfc9945a31783be88ebf246aa2ef51
Riccardo Perra <riccardo.perra@icloud.com>
2021-07-20 21:39:52 +0200

    test: add type guard test

---
+++ projects/ngx-reactive-loading/src/spec/utils/type-guards.spec.ts
@@ -0,0 +1,65 @@
+import { of, pipe } from 'rxjs';
+import {
+  isLoadingStore,
+  isLoadingStoreState,
+  LoadingStore,
+  LoadingStoreState,
+} from '../../lib/model';
+
+describe('loading store typeg guards', () => {
+  it('is loadingStoreState', () => {
+    const obj: LoadingStoreState = {
+      $: of(true),
+      track: pipe,
+    };
+
+    const result = isLoadingStoreState(obj);
+
+    expect(result).toBeTrue();
+  });
+
+  it('is not loadingStoreState', () => {
+    const obj: any = {
+      $: 'true',
+      track: {},
+    };
+
+    const result = isLoadingStoreState(obj);
+
+    expect(result).toBeFalse();
+  });
+
+  it('is loadingStore', () => {
+    const obj: LoadingStore<['prop1', 'prop2']> = {
+      prop1: {
+        $: of(true),
+        track: pipe,
+      },
+      prop2: {
+        $: of(false),
+        track: pipe,
+      },
+    };
+
+    const result = isLoadingStore(obj);
+
+    expect(result).toBeTrue();
+  });
+
+  it('is not loading store', () => {
+    const obj = {
+      prop1: {
+        $: of(true),
+        track: pipe,
+      },
+      prop2: {
+        $: false,
+        track: pipe,
+      },
+    };
+
+    const result = isLoadingStore(obj);
+
+    expect(result).toBeFalse();
+  });
+});

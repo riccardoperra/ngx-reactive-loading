4b87f48575fee229afd9552ff6fc1192365e20ed
Riccardo Perra <riccardo.perra@icloud.com>
2021-07-21 00:24:57 +0200

    test: add provider and loading service test

---
+++ projects/ngx-reactive-loading/src/spec/providers.spec.ts
@@ -0,0 +1,134 @@
+import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { PropertyTuple } from '../lib/model/property';
+import {
+  FEATURE_LOADING_STORE,
+  INITIAL_LOADING_STORE,
+  LOADING_STORE_OPTIONS,
+  PARENT_LOADING_STORE,
+  ROOT_LOADING_STORE,
+} from '../lib/providers/token';
+import { RootReactiveLoadingModule } from '../lib/reactive-loading.module';
+import {
+  LoadingService,
+  LoadingStore,
+  ReactiveLoadingModule,
+} from '../public-api';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { defer } from 'rxjs';
+
+type RootModuleActions = 'add' | 'delete';
+type FeatureModuleActions = 'addFeature' | 'deleteFeature';
+
+describe('Reactive loading module', () => {
+  it('provide root module and root loading service instance', () => {
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveLoadingModule.forRoot<RootModuleActions>(['add', 'delete']),
+      ],
+    });
+
+    const module = TestBed.inject(RootReactiveLoadingModule);
+    expect(module).toBeTruthy();
+
+    const loadingStore = TestBed.inject(LoadingService);
+    assertLoadingStoreState(['add', 'delete'], loadingStore.state);
+  });
+
+  it('should create standalone feature service', () => {
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveLoadingModule.forFeature<FeatureModuleActions>(
+          ['addFeature', 'deleteFeature'],
+          { standalone: true }
+        ),
+      ],
+    });
+
+    const module = TestBed.inject(ReactiveLoadingModule);
+    expect(module).toBeTruthy();
+
+    const loadingStore = TestBed.inject(LoadingService);
+    assertLoadingStoreState(
+      ['addFeature', 'deleteFeature'],
+      loadingStore.state
+    );
+  });
+
+  it('should provide with componentProvider', () => {
+    const componentProviders = LoadingService.componentProvider(
+      ['prop1', 'prop2'],
+      { standalone: true }
+    );
+
+    TestBed.configureTestingModule({
+      providers: [componentProviders],
+    });
+
+    const initialState = TestBed.inject(INITIAL_LOADING_STORE);
+    expect(initialState).toEqual(['prop1', 'prop2']);
+
+    const options = TestBed.inject(LOADING_STORE_OPTIONS);
+    expect(options).toEqual({ standalone: true });
+
+    const parent = TestBed.inject(PARENT_LOADING_STORE);
+    expect(parent).toBeNull();
+  });
+
+  it('should not be standalone', () => {
+    const componentProviders = LoadingService.componentProvider(
+      ['prop1', 'prop2'],
+      { standalone: false }
+    );
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: LoadingService,
+          useValue: new LoadingService([], { standalone: true }, null),
+        },
+        componentProviders,
+      ],
+    });
+
+    const parent = TestBed.inject(PARENT_LOADING_STORE);
+
+    expect(parent).toBeInstanceOf(LoadingService);
+  });
+
+  it('should throw twice .forRoot error', () => {
+    @NgModule({
+      imports: [ReactiveLoadingModule.forRoot([])],
+    })
+    class FeatureModule {}
+
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveLoadingModule.forRoot([]),
+        RouterTestingModule.withRoutes([
+          {
+            path: 'featureModule',
+            loadChildren: () => Promise.resolve(FeatureModule),
+          },
+        ]),
+      ],
+    });
+
+    const router = TestBed.inject(Router);
+
+    defer(() => {
+      return router.navigate(['featureModule']);
+    }).subscribe({
+      error: err => expect(err).toBeInstanceOf(TypeError),
+    });
+  });
+});
+
+function assertLoadingStoreState<T extends PropertyKey>(
+  expected: string[],
+  state: LoadingStore<PropertyTuple<T>>
+) {
+  const keys = Object.keys(state);
+  expect(keys).toEqual(expected);
+}
---
+++ projects/ngx-reactive-loading/src/spec/services/loading.service.spec.ts
@@ -0,0 +1,161 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { LoadingEvent, someLoading } from 'ngx-reactive-loading';
+import { combineLatest, of, timer } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { LoadingService } from '../../public-api';
+import { marbles } from 'rxjs-marbles';
+
+describe('LoadingService', () => {
+  let service: LoadingService;
+
+  beforeEach(() => {
+    service = new LoadingService(
+      ['prop1', 'prop2'],
+      { standalone: true },
+      null
+    );
+  });
+
+  it('should create loading store', () => {
+    const expected = ['prop1', 'prop2'];
+    expect(Object.keys(service.state)).toEqual(expected);
+  });
+
+  it('isLoading', fakeAsync(() => {
+    const spy = jasmine.createSpy();
+
+    service.load(timer(1000), 'prop1').subscribe();
+
+    service.isLoading('prop1').subscribe(spy);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(1000);
+
+    expect(spy).toHaveBeenCalledWith(false);
+  }));
+
+  it('track', fakeAsync(() => {
+    const spy = jasmine.createSpy();
+
+    timer(1000).pipe(service.track('prop1')).subscribe();
+
+    service.isLoading('prop1').subscribe(spy);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(1000);
+
+    expect(spy).toHaveBeenCalledWith(false);
+  }));
+
+  it('should throw key not found on track', fakeAsync(() => {
+    expect(() => {
+      timer(1000).pipe(service.track('notFoundProp')).subscribe();
+    }).toThrowError(Error);
+  }));
+
+  it('someLoading by props', fakeAsync(() => {
+    const spy = jasmine.createSpy();
+
+    combineLatest([
+      service.load(timer(1000), 'prop1'),
+      service.load(timer(2000), 'prop2'),
+    ]).subscribe();
+
+    service.someLoading(['prop1', 'prop2']).subscribe(spy);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(1000);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(2000);
+
+    expect(spy).toHaveBeenCalledWith(false);
+  }));
+
+  it('someLoading by all props', fakeAsync(() => {
+    const spy = jasmine.createSpy();
+
+    combineLatest([
+      service.load(timer(1000), 'prop1'),
+      service.load(timer(2000), 'prop2'),
+    ]).subscribe();
+
+    service.someLoading().subscribe(spy);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(1000);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(2000);
+
+    expect(spy).toHaveBeenCalledWith(false);
+  }));
+
+  it('someLoading with parent', fakeAsync(() => {
+    const spy = jasmine.createSpy();
+    const parentService = new LoadingService(
+      ['parent1'],
+      { standalone: true },
+      null
+    );
+    service = new LoadingService(
+      ['child1'],
+      { standalone: false },
+      parentService
+    );
+
+    combineLatest([
+      parentService.load(timer(1000), 'parent1'),
+      service.load(timer(2000), 'child1'),
+    ]).subscribe();
+
+    service.someLoading(['parent1']).subscribe(spy);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(1000);
+
+    expect(spy).toHaveBeenCalledWith(true);
+
+    tick(2000);
+
+    expect(spy).toHaveBeenCalledWith(false);
+  }));
+
+  it(
+    'would emit a new value for each property changed',
+    marbles(m => {
+      const prop1$ = service.load(of('prop1').pipe(delay(10)), 'prop1');
+      const prop2$ = service.load(of('prop2').pipe(delay(15)), 'prop2');
+
+      m.expect(service.events$).toBeObservable(
+        m.hot('(ab)------c----d', {
+          a: { type: 'prop1', loading: true },
+          b: { type: 'prop2', loading: true },
+          c: { type: 'prop1', loading: false },
+          d: { type: 'prop2', loading: false },
+        })
+      );
+
+      m.expect(prop1$).toBeObservable('----------(a|)', { a: 'prop1' });
+      m.expect(prop2$).toBeObservable('---------------(b|)', { b: 'prop2' });
+    })
+  );
+
+  it('complete when loadingService is destroyed', (doneFn: DoneFn) => {
+    const isLoading = service.someLoading([]);
+
+    isLoading.subscribe({
+      complete: () => doneFn(),
+    });
+
+    service.ngOnDestroy();
+  });
+});

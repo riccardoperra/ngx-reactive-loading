daff8c55179dbeb18ee91b838e2a9e7c963ca5b6
Riccardo Perra <riccardo.perra@icloud.com>
2021-07-21 00:24:23 +0200

    chore: add ci testing script, add testing coverage config

--- projects/ngx-reactive-loading/karma.conf.js
+++ projects/ngx-reactive-loading/karma.conf.js
@@ -25,9 +25,16 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-reactive-loading'),
+      dir: require('path').join(
+        __dirname,
+        '../../coverage/ngx-reactive-loading'
+      ),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,

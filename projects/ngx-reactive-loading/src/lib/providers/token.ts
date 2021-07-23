865cb62cb2366ec280bfd3730cbcd66a713d1e22
Riccardo Perra <riccardo.perra@icloud.com>
2021-07-23 21:44:09 +0200

    fix: export SOME_LOADING token

--- projects/ngx-reactive-loading/src/lib/providers/token.ts
+++ projects/ngx-reactive-loading/src/lib/providers/token.ts
@@ -3,7 +3,3 @@ import { InjectionToken } from '@angular/core';
 export const SOME_LOADING = new InjectionToken<boolean>(
   '[ngx-reactive-loading] loadingService/someLoading'
 );
-
-export const IS_LOADING = new InjectionToken<boolean>(
-  '[ngx-reactive-loading] loadingService/isLoading'
-);
--- projects/ngx-reactive-loading/src/public-api.ts
+++ projects/ngx-reactive-loading/src/public-api.ts
@@ -7,3 +7,4 @@ export { createLoadingStore } from './lib/utils';
 export { someLoading, withLoading } from './lib/operators';
 export { LoadingService } from './lib/services';
 export { ReactiveLoadingModule } from './lib/reactive-loading.module';
+export { SOME_LOADING } from './lib/providers/token';
